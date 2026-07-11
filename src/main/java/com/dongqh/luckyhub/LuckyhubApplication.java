4bd4279ec8233ebbc26ee8aae0ecd842a1ff3dc8
dongqh1 <412274773@qq.com>
2026-07-11 16:31:14 +0800

    11

---
+++ src/main/java/com/dongqh/luckyhub/LuckyhubApplication.java
@@ -0,0 +1,13 @@
+package com.dongqh.luckyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyhubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckyhubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dongqh/luckyhub/LuckyhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongqh.luckyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyhubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
